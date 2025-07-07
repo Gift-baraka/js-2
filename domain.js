a36380bf825e56ae681d3978871cf92340761f06
Gift-baraka <gbshikuku@gmail.com>
2025-07-07 22:21:42 +0300

    Add files via upload

---
+++ domain.js
@@ -0,0 +1,15 @@
+function validatePhoneNumber() {
+      const phone = document.getElementById("phone").value;
+      const error = document.getElementById("error");
+
+      /
+      const pattern = /^07\d{8}$/;
+
+      if (!pattern.test(phone)) {
+        error.textContent = "Please enter a valid Kenyan phone number";
+        return false; 
+      }
+
+      error.textContent = ""; 
+      alert("Granted!");
+      return true; 
